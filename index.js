08af6d0711367a133d7b5d7a4e526bc8cc80699c
haanj <joshua.haan@ymail.com>
2016-03-21 12:48:46 -0700

    creates new users and creates new files for existing users

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ let app = express();
 let mongoose = require('mongoose');
 
 let User = require(__dirname + '/models/user_module.js');
-// let File = require(__dirname + '/models/file_module.js');
+let File = require(__dirname + '/models/file_module.js');
 
-// var s3 = new AWS.S3();
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
 
 mongoose.connect(DB_PORT);
 
@@ -32,45 +33,85 @@ app.route('/users')
     console.log('post /users');
     console.log(req.body);
     var newUser = new User(req.body);
+    newUser.name = newUser.name.replace(' ', '_');
     newUser.save((err, user) => {
       res.json(user);
       console.log('responded, request terminated');
     });
   });
 
-app.route('users/:user')
+app.route('/users/:user')
   .get((req, res) => {
-    console.log('request received');
-    // console.log('get /users/'+req.params.user);
-    res.json('get /users/'+req.params.user);
-    // User.find({name: req.params.user})
-    //   .populate('files')
-    //   .exec((err, user) => {
-    //     if (err) return res.send(err);
-    //     // res.json(user);
-    //     console.log('responded, request terminated');
-    //   });
+    console.log('get /users/'+req.params.user);
+    User.find({name: req.params.user})
+      .populate('files')
+      .exec((err, user) => {
+        if (err) return res.send(err);
+        res.json(user);
+        console.log('responded, request terminated');
+      });
   })
   .put((req, res) => {
     console.log('put /users/'+req.params.user);
-    res.json('put /users/'+req.params.user);
+    User.findOneAndUpdate({name: req.params.user}, req.body, (err, user) => {
+      res.json(user);
+    });
   })
   .delete((req, res) => {
     console.log('delete /users/'+req.params.user);
-    res.json('delete /users/'+req.params.user);
+    User.findOneAndRemove({name: req.params.user}, (err, user) => {
+      res.json(user);
+    });
   });
 
-app.route('users/:user/files')
+app.route('/users/:user/files')
   .get((req, res) => {
     console.log('get /users/'+req.params.user+'/files');
     res.json('get /users/'+req.params.user+'/files');
   })
-  .post((req, res) => {
+  .post((req, res, next) => {
     console.log('post /users/'+req.params.user+'/files');
-    res.json('post /users/'+req.params.user+'/files');
+    req.user = req.params.user;
+    req.fileName = req.body.fileName;
+    req.content = req.body.content;
+    req.key = req.user+'-'+req.fileName; // each user can have a unique fileName
+    addToBucket(req, res, next);
+    addToFile(req, res, next);
+    addToUser(req, res, next);
+    res.json('should be done or whatever');
   });
 
-app.route('files/:file')
+function addToBucket(req, res, next) {
+  let params = {Bucket: 'haanjBucket', Key: req.key, Body: req.content};
+  s3.putObject(params, (err, data) => {
+    req.ETag = data.ETag;
+    console.log(data);
+    next();
+  });
+}
+
+function addToFile(req, res, next) {
+  let fileObj = {name: req.fileName, key: req.key};
+  let newFile = new File(fileObj);
+
+  newFile.save((err, file) => {
+    req.fileId = file._id;
+    console.log(file);
+    next();
+  });
+}
+
+function addToUser(req, res, next) {
+  User.findOne({name: req.params.user}, (err, user) => {
+    user.files.push(req.fileId);
+    user.save((err, user) => {
+      console.log(user);
+      next();
+    });
+  });
+}
+
+app.route('/files/:file')
   .get((req, res) => {
     console.log('get /files/'+req.params.file);
     res.json('get /files/'+req.params.file);
@@ -84,6 +125,8 @@ app.route('files/:file')
     res.json('get /files/'+req.params.file);
   });
 
-app.listen(S_PORT, () => {
-  console.log('server listening at 3000');
-});
+s3.createBucket({Bucket: 'haanjBucket'}, function() {
+  app.listen(S_PORT, () => {
+    console.log('server listening at 3000');
+  });
+})
--- models/user_module.js
+++ models/user_module.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: { type : String, unique : true, required : true, dropDups: true},
+  favorite_color: String,
   files: [{type: mongoose.Schema.Types.ObjectId, ref: 'File'}]
 });
 
